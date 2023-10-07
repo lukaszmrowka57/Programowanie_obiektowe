5049772752ae6d516b66e62660914e3d65bfcc17
lukaszmrowka57 <115405926+lukaszmrowka57@users.noreply.github.com>
2023-10-07 15:57:18 +0200

    Add files via upload

---
+++ zad_2.1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TemperatureConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj temperaturę w stopniach Celsjusza: ");
+
+            
+            string input = Console.ReadLine();
+
+            
+            if (double.TryParse(input, out double celsius))
+            {
+                
+                double fahrenheit = CelsiusToFahrenheit(celsius);
+
+                
+                Console.WriteLine($"{celsius} stopni Celsjusza to {fahrenheit:F2} stopni Fahrenheita.");
+            }
+            else
+            {
+                
+                Console.WriteLine("Proszę wprowadzić prawidłową wartość liczbową.");
+            }
+        }
+
+        
+        static double CelsiusToFahrenheit(double celsius)
+        {
+            return celsius * 9.0 / 5.0 + 32;
+        }
+    }
+}
