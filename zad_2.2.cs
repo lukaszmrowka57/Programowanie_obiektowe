61e854e28d9ae9aae9509f277d02bb4dc54ecd0c
lukaszmrowka57 <115405926+lukaszmrowka57@users.noreply.github.com>
2023-10-07 16:23:14 +0200

    Add files via upload

---
+++ zad_2.2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DeltaCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj a: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Podaj b: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("Podaj c: ");
+            double c = double.Parse(Console.ReadLine());
+
+            double delta = b * b - 4 * a * c;
+            Console.WriteLine($"Delta wynosi: {delta}");
+        }
+    }
+}
