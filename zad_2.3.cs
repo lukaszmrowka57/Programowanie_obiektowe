ee019a978c9047e7927305b16e5b3d3503dfa224
lukaszmrowka57 <115405926+lukaszmrowka57@users.noreply.github.com>
2023-10-22 11:02:59 +0200

    Add files via upload

---
+++ zad_2.3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BMICalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj swoją wagę w kilogramach:");
+            double waga;
+            while (!double.TryParse(Console.ReadLine(), out waga) || waga <= 0)
+            {
+                Console.WriteLine("Proszę podać poprawną wartość dla wagi.");
+            }
+
+            Console.WriteLine("Podaj swój wzrost w centymetrach:");
+            double wzrostWcm;
+            while (!double.TryParse(Console.ReadLine(), out wzrostWcm) || wzrostWcm <= 0)
+            {
+                Console.WriteLine("Proszę podać poprawną wartość dla wzrostu.");
+            }
+            
+            double wzrost = wzrostWcm / 100;  // przeliczenie z cm na m
+
+            double bmi = waga / (wzrost * wzrost);
+            Console.WriteLine($"Twój wskaźnik BMI wynosi: {bmi:F2}.");
+        }
+    }
+}
